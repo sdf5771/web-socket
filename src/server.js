06913e7f9d8e2b769dc4f874b84b56d97bf20ad8
SeobisBack <seobisback@gmail.com>
2024-03-11 20:01:05 +0900

    setting. view engine
    setting pug, route, nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
-console.log('Server Start');
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleAppListen = () => console.log('Server Start');
+
+app.listen(3000, handleAppListen);
