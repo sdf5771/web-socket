68cfc17769f3a1595aaf3be8d83e7dc4c5199909
SeobisBack <seobisback@gmail.com>
2024-03-31 15:51:02 +0900

    feat. connection socket frontend, backend both

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,24 @@
 console.log('front-end js')
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", (event) => {
+    console.log("Connected to Server ✅");
+    console.log("open event ", event);
+})
+
+socket.addEventListener('message', (event) => {
+    console.log("Socket Server : ", event);
+    if(event){
+        setTimeout(() => {
+            socket.send({
+                message: "this message is receive to browser"
+            })
+        }, 2000);
+    }
+})
+
+socket.addEventListener("close", (event) => {
+    console.log("Disconnected from Server ", event);
+})
+
--- src/server.js
+++ src/server.js
@@ -14,7 +14,19 @@ const handleAppListen = () => console.log('Server Start');
 
 // app.listen(3000, handleAppListen);
 
+// Create HTTP Server
 const server = http.createServer(app);
+
+// Create Web Socket Server with http Server
 const webSocketServer = new WebSocket.Server({ server });
+
+webSocketServer.on("connection", (socket) => {
+    console.log("Socket Server : Connected to Browser ✅")
+    socket.on("close", () => console.log("Disconnected from the Browser"));
+    socket.send('socket: send message');
+    socket.on("message", (message) => {
+        console.log("Browser Sending Message :", message);
+    })
+})
 
 server.listen(3000, handleAppListen);
