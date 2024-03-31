f02f54bc3fadafb2d66a6e7b6ae8eb8d64ef3570
SeobisBack <seobisback@gmail.com>
2024-03-31 18:39:16 +0900

    feat. create nickname form with frontend

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,16 +1,27 @@
 console.log('front-end js')
 
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeSendMessage(type, payload){
+    const msg = {type, payload};
+
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", (event) => {
     console.log("Connected to Server ✅");
     console.log("open event ", event);
 })
 
-socket.addEventListener('message', (event) => {
-    console.log("Socket Server : ", event.data);
+socket.addEventListener('message', (message) => {
+    console.log("Socket Server : ", message.data);
+    const li = document.createElement("li");
+
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", (event) => {
@@ -20,8 +31,16 @@ socket.addEventListener("close", (event) => {
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeSendMessage("new_message", input.value));
     input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeSendMessage("nickname", input.value));
+}
+
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
